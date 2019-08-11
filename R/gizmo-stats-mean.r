8431b5348a34f02c25a578a2c3be4c54dd9dadac
sw3023 <sw3023@columbia.edu>
2019-08-10 17:33:13 -0700

    Update gizmo-stats-mean.r

--- R/gizmo-stats-mean.r
+++ R/gizmo-stats-mean.r
@@ -2,27 +2,47 @@
 gizmo_stats_mean_ui <- function(ns){
   fluidPage(h4("Mean: A Stats Tool"),
             fluidRow(
-              column(4,textInput(ns("stats_mean_name"), "string name","John")),
-              column(4,textInput(ns("stats_mean"), "string","My name is Johnny"))
+              column(4,textInput(ns("stats_mean_name"), "Assigned to name ...", "biostats_Age_mean")),
+              column(4,checkboxInput(ns("stats_mean_auto"), "auto generate name", value = TRUE))
+            ),
+            fluidRow(
+              column(4,textInput(ns("stats_mean_df"), "Enter data.frame or matrix", "biostats")),
+              column(4,textInput(ns("stats_mean_col"), "Enter column", "Age"))
             )
   )
 }
 
 gizmo_stats_mean_server <- function(input, output, session, state=NULL){
 
+
+  obsB <- observeEvent(c(input$stats_mean_df, input$stats_mean_col, input$stats_mean_name),{
+    if(input[["stats_mean_auto"]]){
+	   message("B")
+      updateTextInput(session, "stats_mean_name", value=paste0(input[["stats_mean_df"]], "_", input[["stats_mean_col"]],"_stats_mean"))
+    }
+  }, ignoreInit=TRUE, suspended=TRUE)
+
   # Restore UI state
   if (!is.null(state)) {
     session$onFlushed(function() {
+      updateCheckboxInput(session, "stats_mean_auto", value=state$stats_mean_auto)
+      updateTextInput(session, "stats_mean_df", value=state$stats_mean_df)
+      updateTextInput(session, "stats_mean_col", value=state$stats_mean_col)
       updateTextInput(session, "stats_mean_name", value=state$stats_mean_name)
-      updateTextInput(session, "stats_mean", value=state$stats_mean)
+	  obsB$resume() #observeEvent resumes after loading states
+	  message("A")
     })
+  }else{
+	obsB$resume()
   }
+    
 
   # RMarkdown Code
   txt_react <- reactive({
-    txt <- paste0("<!-- user may enter a string into global environment -->", "\n",
-                  "```{r} \n",
-                  input[["stats_mean_name"]]," <- ", "'", input[["stats_mean"]] , "'" , "\n",
+    txt <- paste0("```{r} \n",
+                  "print(", input[["stats_mean_df"]], "[['", input[["stats_mean_col"]], "']]", ")\n",
+                  input[["stats_mean_name"]], " <- mean(", input[["stats_mean_df"]], "[['", input[["stats_mean_col"]], "']]", ")\n",
+                  "print(", input[["stats_mean_name"]], ")\n",
                   "```\n")
     txt
   })
@@ -30,8 +50,10 @@ gizmo_stats_mean_server <- function(input, output, session, state=NULL){
   # Get UI state
   get_state <- function(){
     list(
-      stats_mean=input[["stats_mean"]],
       stats_mean_name=input[["stats_mean_name"]],
+      stats_mean_auto=input[["stats_mean_auto"]],
+      stats_mean_df=input[["stats_mean_df"]],
+      stats_mean_col=input[["stats_mean_col"]],
       `__version__` = "0.0.1"
     )
   }
